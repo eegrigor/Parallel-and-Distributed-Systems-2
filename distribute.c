9aefad5f8d8e82858eb7aaa68fc0dde875c49d78
eegrigor <eegrigor@gmail.com>
2021-12-01 18:14:17 +0000

    Update Files

---
+++ distribute.c
@@ -0,0 +1,20 @@
+/*
+* Author: * Efthymios Grigorakis - 9694
+
+ * 2021 Aristotle University of Thessaloniki
+ * Parallel and Distributed Systems.
+*/
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void distributeByMedian(void param){
+
+}
+
+
+int main(int argc, char **argv){
+
+    return 0 ;
+}
